a62b92884ba20ac5d215467055bb8bfd36acb83c
simialbi <simi.albi@gmail.com>
2018-05-09 09:48:21 +0200

    added yii2 coding standards and reformatted code format initial value automatically before init js plugin

--- DatetimepickerAsset.php
+++ DatetimepickerAsset.php
@@ -13,39 +13,40 @@ use simialbi\yii2\web\AssetBundle;
  *
  * @author Simon Karlen <simi.albi@gmail.com>
  */
-class DatetimepickerAsset extends AssetBundle {
-	/**
-	 * @var string the directory that contains the source asset files for this asset bundle.
-	 */
-	public $sourcePath = '@bower/eonasdan-bootstrap-datetimepicker/build';
+class DatetimepickerAsset extends AssetBundle
+{
+    /**
+     * @var string the directory that contains the source asset files for this asset bundle.
+     */
+    public $sourcePath = '@bower/eonasdan-bootstrap-datetimepicker/build';
 
-	/**
-	 * @var array list of CSS files that this bundle contains.
-	 */
-	public $css = [
-		'css/bootstrap-datetimepicker.min.css'
-	];
+    /**
+     * @var array list of CSS files that this bundle contains.
+     */
+    public $css = [
+        'css/bootstrap-datetimepicker.min.css'
+    ];
 
-	/**
-	 * @var array list of JavaScript files that this bundle contains.
-	 */
-	public $js = [
-		'js/bootstrap-datetimepicker.min.js'
-	];
+    /**
+     * @var array list of JavaScript files that this bundle contains.
+     */
+    public $js = [
+        'js/bootstrap-datetimepicker.min.js'
+    ];
 
-	/**
-	 * @var array list of bundle class names that this bundle depends on.
-	 */
-	public $depends = [
-		'simialbi\yii2\web\MomentAsset',
-		'yii\bootstrap\BootstrapPluginAsset'
-	];
+    /**
+     * @var array list of bundle class names that this bundle depends on.
+     */
+    public $depends = [
+        'simialbi\yii2\web\MomentAsset',
+        'yii\bootstrap\BootstrapPluginAsset'
+    ];
 
-	/**
-	 * @var array the options to be passed to [[AssetManager::publish()]] when the asset bundle
-	 * is being published.
-	 */
-	public $publishOptions = [
-		'forceCopy' => YII_DEBUG
-	];
+    /**
+     * @var array the options to be passed to [[AssetManager::publish()]] when the asset bundle
+     * is being published.
+     */
+    public $publishOptions = [
+        'forceCopy' => YII_DEBUG
+    ];
 }
